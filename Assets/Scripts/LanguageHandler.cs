09e2216755ceb0133693438fe426b059392ecb64
Levi Buck <levi.buck222@gmail.com>
2023-07-22 11:42:19 +0100

    Translation init

---
+++ Assets/Scripts/LanguageHandler.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Windows.WebCam;
+
+public class LanguageHandler : MonoBehaviour
+{
+    [SerializeField] private TextAsset fullTranslation;
+    private LanguageType curSelectedLanguage = LanguageType.English;
+    private const string LANGUAGE_KEY = "CurLanguageKey";
+
+    public static LanguageHandler Instance;
+    private Dictionary<string, string> translations = new Dictionary<string, string>();
+
+    private void Awake()
+    {
+        if ( Instance == null )
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy( this );
+        }
+
+        ReadTextFile();
+        ReloadLanguage();
+    }
+
+    private void ReadTextFile()
+    {
+        var document = fullTranslation.text.Split( '\t', '\n' );
+        var rowSize = fullTranslation.text.Split( '\n' )[ 0 ].Split( '\t' ).Length;
+        var columnSize = document.Length / rowSize;
+
+        for ( int i = 1; i < rowSize; i++ )
+        {
+            for ( int j = 0; j < columnSize; j++ )
+            {
+                var content = document[ i + rowSize * j ];
+                var stringID = document[ j * rowSize ] + i;
+                translations.Add( stringID.ToLower(), content );
+            }
+        }
+    }
+
+    public string GetTranslation( string stringID )
+    {
+        var fullStringID = stringID + ( int )curSelectedLanguage;
+        return translations[ fullStringID.ToLower() ];
+    }
+
+    private void ReloadLanguage( bool needsReloading = false )
+    {
+        if ( PlayerPrefs.HasKey( LANGUAGE_KEY ) )
+        {
+            curSelectedLanguage = ( LanguageType )PlayerPrefs.GetInt( LANGUAGE_KEY );
+        }
+        else
+        {
+            curSelectedLanguage = LanguageType.English;
+        }
+
+        if ( needsReloading )
+        {
+            SceneManager.LoadScene( SceneManager.GetActiveScene().buildIndex );
+        }
+    }
+
+    private void SelectNewLanguage( LanguageType languageType, bool needsReloading = false )
+    {
+        curSelectedLanguage = languageType;
+        PlayerPrefs.SetInt( LANGUAGE_KEY, ( int )languageType );
+        ReloadLanguage( needsReloading );
+    }
+
+    public void ChangeToDutch()
+    {
+        SelectNewLanguage(LanguageType.Dutch,true);
+    }
+
+    public void ChangeToFrench()
+    {
+        SelectNewLanguage( LanguageType.French, true );
+    }
+
+    public void ChangeToEnglish()
+    {
+        SelectNewLanguage( LanguageType.English, true );
+    }
+
+}
+
+public enum LanguageType
+{
+    English = 1,
+    Dutch = 2,
+    French = 3,
+}
---
+++ Assets/Scripts/Translator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class Translator : MonoBehaviour
+{
+    private TextMeshProUGUI textMeshText;
+
+    private void Start()
+    {
+        GetReference();
+        ReplaceTextWithTranslatedText();
+    }
+
+    private void GetReference()
+    {
+        textMeshText = GetComponent<TextMeshProUGUI>();
+    }
+
+    private string GetStringID()
+    {
+        if ( textMeshText != null )
+        {
+            return textMeshText.text;
+        }
+        
+        Debug.LogError($"Get stringID failed on the: {gameObject.name} GameObject. Expect errors in the UI translations. Are you sure the given text is not empty?");
+        return "error :(";
+    }
+
+    private void ReplaceTextWithTranslatedText()
+    {
+        var text = LanguageHandler.Instance.GetTranslation( GetStringID() );
+        if ( textMeshText != null )
+        {
+            textMeshText.text = text;
+        }
+    }
+}
